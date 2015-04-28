f8e8bf0f0570e703a9fda7c14667b2b42414c95d
Neil Blakey-Milner <nbm@nxsy.org>
2015-04-27 20:05:07 -0700

    Add first Hello World module

---
+++ plugins/HelloWorld/HelloWorld/HelloWorldModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class HelloWorldModule : MonoBehaviour
+    {
+        void Awake()
+        {
+            Debug.Log("ksp-example-plugins: HelloWorldModule: Hello!");
+        }
+    }
+}
